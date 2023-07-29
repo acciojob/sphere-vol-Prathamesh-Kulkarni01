275848c954992c40f9e025f10f3ac35977888ddf
Prathamesh Kulkarni <65341679+Prathamesh-Kulkarni01@users.noreply.github.com>
2023-07-29 13:10:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function volume_sphere() {
+
+function volume_sphere(e) {
+	e.preventDefault()
     //Write your code here
-  
+	const r=document.getElementById("radius").value
+	const v=document.getElementById("volume")
+	v.value=r
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
